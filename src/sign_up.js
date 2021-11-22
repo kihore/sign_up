b97a99a79f7eb6441fdc449c8126e436ffa7e6b4
kihore <kannankishore20@gmail.com>
2021-11-22 22:53:20 +0530

    commit on master

--- src/sign_up.js
+++ src/sign_up.js
@@ -1,10 +1,25 @@
-import React,{useState} from 'react'
+import React,{useState}from 'react'
 
-const sign_up = () => {
+function sign_up() {
+const [data, setData] = useState([])
+useState (() => {
+    fetch("/api/data").then(
+        res => setData(res.data)
+    )
+},[])
+    
+}
+const sign_up = ({name,email,password,confirm_password}) => {
     return (
-        <div>
+        <section className= 'signto'>
+          <input type= 'text' input = 'Name' placeholder = 'user name' />
+          <input type= 'text' input = 'email' placeholder = 'enter email-id' />
+          <input type= 'text' input = 'Password' placeholder = 'password' />
+          <input type= 'text' input = 'confirm_Password' placeholder = 'confrim password' />
+          <input type = 'button' onClick = 'submit'/>
+          </section>
             
-        </div>
+       
     )
 }
 
